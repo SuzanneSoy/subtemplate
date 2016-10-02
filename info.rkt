1995dae967579b7e3b3da0b24a9ce77328e8c94b
Georges Dupéron <georges.duperon@gmail.com>
2016-10-02 03:57:53 +0200

    Added some basic dependencies

--- info.rkt
+++ info.rkt
@@ -1,9 +1,14 @@
 #lang info
 (define collection "phc-graph")
 (define deps '("base"
-               "rackunit-lib"))
+               "rackunit-lib"
+               "phc-toolkit"
+               "phc-adt"
+               "type-expander"
+               "hyper-literate"
+               "scribble-enhanced"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/phc-graph.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
-(define pkg-authors '(georges))
+(define pkg-authors '("Georges Dupéron"))
