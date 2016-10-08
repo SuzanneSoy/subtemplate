3eecf3796e83126190e77516b9658f440859b6d8
Georges Dupéron <georges.duperon@gmail.com>
2016-10-08 14:53:20 +0200

    Hopefully fixed order of union elements in the cond

--- dispatch-union.rkt
+++ dispatch-union.rkt
@@ -3,14 +3,14 @@
 (require racket/require
          phc-toolkit
          phc-adt
-         (for-syntax (subtract-in racket/base "subtemplate.rkt")
+         (for-syntax racket/base
                      phc-toolkit/untyped
                      racket/syntax
-                     (subtract-in syntax/parse "subtemplate.rkt")
+                     racket/format
+                     syntax/parse
                      syntax/parse/experimental/template
                      type-expander/expander
-                     "free-identifier-tree-equal.rkt"
-                     "subtemplate.rkt")
+                     "free-identifier-tree-equal.rkt")
          (for-meta 2 racket/base)
          (for-meta 2 phc-toolkit/untyped)
          (for-meta 2 syntax/parse))
@@ -19,25 +19,35 @@
 
 (define-syntax/parse (dispatch-union v
                                      ([type-to-replaceᵢ Aᵢ predicateᵢ] …)
-                                     [Xⱼ result] …)
+                                     [Xⱼ resultⱼ] …)
+  (define-syntax-class to-replace
+    (pattern [t result]
+             #:with (_ predicate)
+             (findf (λ (r) (free-identifier-tree=? #'t (stx-car r)))
+                    (syntax->list
+                     #'([type-to-replaceᵢ predicateᵢ] …)))
+             #:with clause #`[(predicate v) result]))
+  
+  (define-syntax-class tagged
+    #:literals (tagged)
+    (pattern [(tagged name [fieldₖ (~optional :colon) typeₖ] …) result]
+             #:with clause #`[((tagged? name fieldₖ …) v) result]))
+
+  (define-syntax-class other
+    (pattern [other result]
+             #:with clause #`[else result]))
+
+  
   ((λ (x) (local-require racket/pretty) #;(pretty-write (syntax->datum x)) x)
-   (quasisyntax/top-loc stx
-     (cond
-       ;; TODO: put first the type-to-replaceᵢ, then afterwards the other Xⱼ, otherwise it can fail to typecheck.
-       . #,(stx-map
-            (λ (Xⱼ result)
-              (syntax-parse Xⱼ
-                #:literals (tagged)
-                [t
-                 #:with (_ predicate)
-                 (findf (λ (r) (free-identifier-tree=? #'t (stx-car r)))
-                        (syntax->list
-                         (subtemplate ([type-to-replaceᵢ predicateᵢ] …))))
-                 #`[(predicate v) #,result]]
-                [(tagged name [fieldₖ (~optional :colon) typeₖ] …)
-                 #`[((tagged? name fieldₖ …) v) #,result]]
-                [other (raise-syntax-error 'graph
-                                           "Unhandled union type"
-                                           #'other)]))
-            #'(Xⱼ …)
-            #'(result …))))))
+   (syntax-parse #'([Xⱼ resultⱼ] …)
+     [({~or to-replace:to-replace
+            tagged:tagged
+            {~between other:other 0 1
+                      #:too-many (~a "only one non-tagged type can be part of"
+                                     " the union")}}
+       …)
+      (quasisyntax/top-loc stx
+        (cond 
+          to-replace.clause …
+          tagged.clause …
+          other.clause …))])))
