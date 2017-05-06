eee77ec3c0611ba768ec63284f0515fe66c0418e
Georges Dupéron <georges.duperon@gmail.com>
2017-05-06 03:43:41 +0200

    Fixed test for #5

--- test/bug-5.rkt
+++ test/bug-5.rkt
@@ -1,20 +1,22 @@
 #lang racket/base
 (require subtemplate/override
          rackunit)
-(check-equal? (let ()
+(check-equal?(syntax->datum
+              (let ()
                 (define/syntax-parse ({~optional
                                        {~or k:keyword b:boolean i:nat}}
                                       {~and {~or (v …) s:str}} …)
                   #'(#:a-keyword (1 2 3 4) "foo" (5 6)))
-                #'(l (?@@ (?? (v …)) …)))
-              '(l 1 2 3 4 5 6))
+                #'(l (?@@ (?? (v …)) …))))
+             '(l 1 2 3 4 5 6))
 
 
-(check-equal? (let ()
-                (define/syntax-parse ({~optional
-                                       {~or k:keyword b:boolean i:nat}}
-                                      {~and {~or (v …) s:str}} …)
-                  #'(#:a-keyword (1 2 3 4) "foo" (5 6)))
-                #'(l (?@@ (?? (v …)) …)))
+(check-equal? (syntax->datum
+               (let ()
+                 (define/syntax-parse ({~optional
+                                        {~or k:keyword b:boolean i:nat}}
+                                       {~and {~or (v …) s:str}} …)
+                   #'(#:a-keyword (1 2 3 4) "foo" (5 6)))
+                 #'(l (?@@ (?? (v …)) …))))
               '(l 1 2 3 4 5 6))
 
